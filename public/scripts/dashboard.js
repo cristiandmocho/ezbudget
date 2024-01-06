c7cf9f0c0910224d78da5b77efd14ee8831a7718
Cristian Mocho <cristian.mocho@ezsystems.net>
2024-01-06 20:01:20 +0000

    Adding charts

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,15 +1,12 @@
 import { ONEDAY } from "./utils/constants.js";
 import {
-  ezDialog,
   formatAsCurrency,
   formatDate,
-  getMonthNames,
   showConfirmDialog,
   showToast,
 } from "./utils/utilities.js";
 
 (async () => {
-  let selectedRow = null;
   let tableData = [];
 
   const dataRow = (row) => {
@@ -60,6 +57,96 @@ import {
         "danger",
         "cancel"
       );
+    }
+  }
+
+  async function loadChartData() {
+    const dataExpenses = [
+      ["Rent", 900],
+      ["Groceries", 650],
+      ["Food (out)", 250],
+      ["Health & Pharma", 5],
+      ["Education", 84 + 1.46 * 23 + 0], // Apoio Júlia + Almoço Francisco + Custos Júlia
+      ["Transport", 5 + 50 + 28], // Bolt + Uber + TUB
+      ["Utilities", 235], // Water + Electricity + Internet + TV
+      ["Leisure", 75 + 4.99 + 11 + 15], // Geral + Amazon Prime + Netflix + XBox
+      ["Taxes", 0.1 * 2200],
+      ["Company Cost", 15.35 + 42.3 + 9 + 10 + 6 + 16.85 + 84.22 + 28.7], // GSuite + Adobe + Copilot + Midjourney + DigitalOcean + B2B + BTCompliance + Contabilidade
+      ["Debts", 0],
+      ["Others", 0],
+    ];
+    const totalExpenses = dataExpenses.reduce((a, b) => a + b[1], 0);
+
+    google.charts.load("current", {
+      packages: ["corechart"],
+      language: "pt-PT",
+    });
+
+    google.charts.setOnLoadCallback(drawExpensesChart);
+    google.charts.setOnLoadCallback(drawIncomeVsExpensesChart);
+
+    function drawExpensesChart() {
+      var data = new google.visualization.DataTable();
+      data.addColumn("string", "Category");
+      data.addColumn("number", "Value");
+      data.addRows(dataExpenses);
+
+      var options = {
+        backgroundColor: "transparent",
+        legend: {
+          position: "right",
+          textStyle: {
+            color: "white",
+            fontName: "Raleway",
+            fontSize: 12,
+          },
+        },
+        title: "Expenses Jan/2024",
+        titleTextStyle: {
+          color: "white",
+          fontName: "Raleway",
+          fontSize: 18,
+          bold: false,
+        },
+      };
+
+      var chart = new google.visualization.PieChart(
+        document.getElementById("chart-expenses")
+      );
+      chart.draw(data, options);
+    }
+
+    function drawIncomeVsExpensesChart() {
+      var data = google.visualization.arrayToDataTable([
+        [
+          "Category",
+          { label: "Amount", format: "currency", type: "number" },
+          { role: "style" },
+        ],
+        ["Income", 3300.0, "color: #008000"],
+        ["Expenses", totalExpenses, "color: #ff0000"],
+      ]);
+
+      var options = {
+        legend: "none",
+        backgroundColor: "transparent",
+        title: "Expenses vs Income",
+        titleTextStyle: {
+          color: "white",
+          fontName: "Raleway",
+          fontSize: 18,
+          bold: false,
+        },
+        vAxis: {
+          minValue: 0,
+          format: "currency",
+        },
+      };
+
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById("chart-income")
+      );
+      chart.draw(data, options);
     }
   }
 
@@ -137,321 +224,6 @@ import {
     tr.classList.add("selected");
   }
 
-  async function openNewInvoiceDialog() {
-    const dlg = ezDialog({
-      title: "New Invoice",
-      content: document.querySelector("#dlgNewInvoice").innerHTML,
-      buttons: [
-        { text: "Save", variant: "primary", name: "btnSave" },
-        { text: "Cancel", variant: "danger", name: "btnCancel" },
-      ],
-      width: "810px",
-    });
-
-    dlg.show();
-
-    // Invoice dialog stuff
-    const currentDate = new Date();
-    const customersData = await fetch("/api/customers").then((data) =>
-      data.json()
-    );
-
-    const invoiceData = {
-      invoice_no: null,
-      created: formatDate(new Date()),
-      due_date: formatDate(new Date().valueOf() + ONEDAY * 15),
-      paid_on: null,
-      customer_uid: null,
-      sent: null,
-      details: "",
-      reference: {
-        month: new Date().getMonth(),
-        year: new Date().getFullYear(),
-      },
-    };
-    const invoiceLines = [];
-
-    let firstLineId = null;
-    let total = 0;
-
-    async function saveInvoice() {
-      if (!invoiceData.customer_uid) {
-        showToast("Please select a customer", "Error", "danger", "cancel");
-        return;
-      }
-
-      if (invoiceLines.length === 0) {
-        showToast("Please add at least one line", "Error", "danger", "cancel");
-        return;
-      }
-
-      if (!invoiceData.reference.month || !invoiceData.reference.year) {
-        showToast(
-          "Please select a reference month and year",
-          "Error",
-          "danger",
-          "cancel"
-        );
-        return;
-      }
-
-      if (!invoiceData.due_date) {
-        showToast("Please select a due date", "Error", "danger", "cancel");
-        return;
-      }
-
-      if (!invoiceData.created) {
-        showToast("Please select a creation date", "Error", "danger", "cancel");
-        return;
-      }
-
-      const data = {
-        invoice: { ...invoiceData },
-        lines: [...invoiceLines],
-      };
-
-      try {
-        await fetch("/api/invoice", {
-          method: "POST",
-          body: JSON.stringify(data),
-          headers: { "Content-Type": "application/json" },
-        });
-
-        showToast(
-          "Successfully created new invoice",
-          "Success",
-          "success",
-          "check"
-        );
-
-        dlg.close();
-
-        loadTableData();
-      } catch (e) {
-        showToast(
-          `Failed to create new invoice. ${e.message}`,
-          "Error",
-          "danger",
-          "cancel"
-        );
-      }
-    }
-
-    function addLine({
-      description = "",
-      quantity = 1,
-      price = 0,
-      discount = 0,
-      vat = 0,
-    }) {
-      const _id = new Date().valueOf();
-      invoiceLines.push({
-        _id,
-        description,
-        quantity,
-        price,
-        discount,
-        vat,
-      });
-
-      return _id;
-    }
-
-    function handleLineValuesChange(e) {
-      const tr = e.currentTarget;
-      const line = invoiceLines.find(
-        (line) => line._id.toString() === tr.getAttribute("data-id")
-      );
-      const attr = e.target.name;
-      const value = e.target.value;
-
-      if (line) {
-        line[attr] = value;
-        updateLine(line._id, line);
-      }
-    }
-
-    function handleInvoiceChanges(e) {
-      const attr = e.target.name;
-      const value = e.target.value;
-
-      invoiceData[attr] = value;
-    }
-
-    function handleRefChange(e) {
-      if (e.target.name === "month")
-        invoiceData.reference.month = Number(e.target.value);
-      else invoiceData.reference.year = Number(e.target.value);
-
-      updateFirstLine();
-    }
-
-    function handleCustomerChange(e) {
-      invoiceData.customer_uid = e.target.value;
-      updateFirstLine();
-    }
-
-    async function updateFirstLine() {
-      if (invoiceLines.length === 0) {
-        firstLineId = addLine({});
-        dlg
-          .querySelector("table tbody tr:first-child")
-          .setAttribute("data-id", firstLineId);
-      }
-
-      if (
-        !invoiceData.customer_uid ||
-        !invoiceData.reference.month ||
-        !invoiceData.reference.year
-      )
-        return;
-
-      const types = {
-        D: "Daily",
-        H: "Hourly",
-        F: "Fixed montlhy",
-      };
-
-      const firstLine = invoiceLines.find((line) => line._id === firstLineId);
-      const customer = customersData.find(
-        (cus) => cus.uid === invoiceData.customer_uid
-      );
-      const calendarInfo = await fetch(
-        `/api/customer/${customer.uid}/calendar?month=${invoiceData.reference.month}&year=${invoiceData.reference.year}`
-      ).then((data) => data.json());
-
-      const referenceText = `${
-        getMonthNames("en")[invoiceData.reference.month]
-      } ${invoiceData.reference.year}`;
-
-      invoiceData.invoice_no = `${invoiceData.reference.year}${(
-        invoiceData.reference.month + 1
-      )
-        .toString()
-        .padStart(2, "0")}${customer.customer_index
-        .toString()
-        .padStart(2, "0")}01`;
-
-      updateLine(firstLineId, {
-        description: `Consulting services ref. ${referenceText} - ${
-          types[customer.type]
-        } rate`,
-        quantity: customer.type === "F" ? 1 : calendarInfo.length,
-        price: customer.rate,
-        discount: firstLine.discount ?? 0,
-        vat: customer.vat,
-      });
-    }
-
-    function updateLine(id, data) {
-      const line = invoiceLines.find((line) => line._id === id);
-      Object.assign(line, data);
-
-      // Shows values
-      const tr = dlg.querySelector(`[data-id="${id}"]`);
-      tr.querySelector("[name='description']").value = line.description;
-      tr.querySelector("[name='quantity']").value = line.quantity;
-      tr.querySelector("[name='price']").value = line.price;
-      tr.querySelector("[name='discount']").value = line.discount;
-      tr.querySelector("[name='vat']").value = line.vat;
-
-      // Shows total
-      total = invoiceLines.reduce((acc, line) => {
-        return (
-          acc +
-          line.quantity * line.price -
-          line.discount +
-          (line.vat / 100) * line.quantity * line.price
-        );
-      }, 0);
-
-      dlg.querySelector(".total").innerText = formatAsCurrency(total);
-    }
-
-    // Dialog initialization
-    updateFirstLine();
-
-    const customerSelect = dlg.querySelector("[name='customer_uid']");
-    const monthSelect = dlg.querySelector("[name='month']");
-    const yearInput = dlg.querySelector("[name='year']");
-
-    const detailsInput = dlg.querySelector("[name='details']");
-    const createdInput = dlg.querySelector("[name='created']");
-    const dueDateInput = dlg.querySelector("[name='due_date']");
-    const paidOnInput = dlg.querySelector("[name='paid_on']");
-    const sentInput = dlg.querySelector("[name='sent']");
-
-    // Initial values
-    customerSelect.innerHTML = "";
-
-    const option = document.createElement("option");
-    option.value = "";
-    option.disabled = true;
-    option.selected = true;
-    option.innerText = "-- Please select --";
-    customerSelect.appendChild(option);
-
-    customersData?.forEach((customer) => {
-      const option = document.createElement("option");
-      option.value = customer.uid;
-      option.innerText = customer.name;
-
-      customerSelect.appendChild(option);
-    });
-
-    const monthNames = getMonthNames("en", "long");
-
-    monthSelect.innerHTML = "";
-    monthNames.forEach((name, index) => {
-      const option = document.createElement("option");
-      option.value = index;
-      option.innerText = name;
-
-      if (index === currentDate.getMonth()) option.selected = true;
-
-      monthSelect.appendChild(option);
-    });
-
-    yearInput.value = currentDate.getFullYear();
-
-    createdInput.value = formatDate(currentDate);
-    dueDateInput.value = formatDate(currentDate.valueOf() + ONEDAY * 15);
-
-    handleInvoiceChanges({ target: createdInput });
-    handleInvoiceChanges({ target: dueDateInput });
-
-    // Dialog events
-    dlg.addEventListener("buttonclick", (e) => {
-      if (e.detail.name === "btnSave") {
-        return saveInvoice();
-      }
-
-      dlg.close();
-    });
-
-    dlg
-      .querySelector("[name='customer_uid']")
-      .addEventListener("change", handleCustomerChange);
-
-    dlg
-      .querySelector("[name='month']")
-      .addEventListener("change", handleRefChange);
-
-    dlg
-      .querySelector("[name='year']")
-      .addEventListener("change", handleRefChange);
-
-    dlg
-      .querySelector("table input[type=number]")
-      .addEventListener("change", handleLineValuesChange);
-
-    detailsInput.addEventListener("change", handleInvoiceChanges);
-    createdInput.addEventListener("change", handleInvoiceChanges);
-    dueDateInput.addEventListener("change", handleInvoiceChanges);
-    paidOnInput.addEventListener("change", handleInvoiceChanges);
-    sentInput.addEventListener("change", handleInvoiceChanges);
-  }
-
   async function updateRow(row) {
     const { uid, status } = row;
 
@@ -527,7 +299,8 @@ import {
   }
 
   // Initializing
-  loadTableData();
+  // loadTableData();
+  loadChartData();
 
   // DOM Events
   document.querySelector("table").addEventListener("click", (e) => {
